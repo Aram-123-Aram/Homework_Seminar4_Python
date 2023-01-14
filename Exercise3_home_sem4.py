9c1ce7591a74f30d0a2df4e78f0be10839c19a05
Aram <armsir-8182@mail.ru>
2023-01-14 22:52:09 +0300

    Doing new list without repeating elements

--- Exercise3_home_sem4.py
+++ Exercise3_home_sem4.py
@@ -1,48 +1,19 @@
 '''
-Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
-Пример:
-- для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+Задайте последовательность чисел. Напишите программу, 
+которая выведет список неповторяющихся элементов исходной последовательности.
 '''
-'''
-#variant1
-k = int(input("Enter the number: k= "))
-list=[]
-n1,n2=1,-1
-for i in range(k-2):
-    i=n1-n2
-    list+=[i]
-    n1=n2
-    n2=i
-for x in reversed(list):
-    print(x, end=" ")
-print(-1,1,end=" ")
-n1,n2=0,1
-print(n1,n2,end=" ")
-for i in range(2,k+1):
-    i=n1+n2
-    print(i, end=" ")
-    n1=n2
-    n2=i
-'''
-#variant2    [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
-k = int(input("Enter the number: k= "))
-
-list=[1, 0, 1]
+import random
 list1=[]
-for i in range(k-1):
-    list1+=[list[1]+list[2]]
-    list[1]=list[2]
-    list[2]=list1[i]
 
-list=[1, 0, 1]
+for i in range(10):
+    n = random.randint(0,10)
+    list1.append(n)
+print("list1= ", list1)
+
 list2=[]
-for i in range(k-1):
-    list2+=[list[1]-list[0]]
-    list[1]=list[0]
-    list[0]=list2[i]
-
-list=[1, 0, 1]
-list2.reverse()
-list2.extend(list)
-list2.extend(list1)
-print(f"k={k}  ->  Fibonachi numbers= {list2}")
+for x in list1:
+    for y in list2:
+        if x==y:
+            list2.remove(y)
+    list2+=[x]
+print("list2= ", list2)
