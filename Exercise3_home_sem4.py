c7b44db4494a460b29e3bb750f713ee008d25708
Aram <armsir-8182@mail.ru>
2023-01-16 21:08:37 +0300

    Showing new list with none-repeating elements in list1

--- Exercise3_home_sem4.py
+++ Exercise3_home_sem4.py
@@ -8,12 +8,20 @@ list1=[]
 for i in range(10):
     n = random.randint(0,10)
     list1.append(n)
-print("list1= ", list1)
+print("      list1= ", list1)
 
 list2=[]
-for x in list1:
-    for y in list2:
-        if x==y:
-            list2.remove(y)
-    list2+=[x]
-print("list2= ", list2)
+list_result=[]
+for i in range(len(list1)):
+    flag=True
+    list2.append(list1[i])
+    for j in range(len(list2)-1):
+        if list1[i]==list2[j]:
+            if list1[i] in list_result:
+               list_result.remove(list1[i])
+            flag=False
+    if flag==True:
+       list_result.append(list1[i])
+    else:
+        flag=True
+print("list_result= ", list_result)
